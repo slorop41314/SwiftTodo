d168e02ac1fcf4f37d26f6ef601c401c567ca054
slorop41314 <albertstanleyy@gmail.com>
2020-10-05 15:36:11 +0700

    Initial Commit

---
+++ ticktickclone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ticktickclone
+//
+//  Created by Albert Stanley on 05/10/20.
+//  Copyright © 2020 slorop. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
